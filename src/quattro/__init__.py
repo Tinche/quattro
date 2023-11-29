76c0ffa7522eb1ec475fc6698cb904f14ef8ddd2
Tin Tvrtković <tinchester@gmail.com>
2023-11-29 01:38:32 +0100

    Fix types

--- src/quattro/__init__.py
+++ src/quattro/__init__.py
@@ -1,14 +1,8 @@
 """Task control for asyncio."""
 from __future__ import annotations
 
-import sys
-from collections.abc import Coroutine, Generator
+from collections.abc import Coroutine
 from typing import Any, Literal, TypeVar, overload
-
-try:
-    from typing import TypeAlias
-except ImportError:
-    from typing_extensions import TypeAlias
 
 from .cancelscope import (
     CancelScope,
@@ -50,23 +44,18 @@ _T4 = TypeVar("_T4")
 _T5 = TypeVar("_T5")
 _T6 = TypeVar("_T6")
 
-if sys.version_info >= (3, 12):
-    _CoroutineLike: TypeAlias = Coroutine[Any, Any, _T]
-else:
-    _CoroutineLike: TypeAlias = "Generator[Any, None, _T] | Coroutine[Any, Any, _T]"
-
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    coro: _CoroutineLike[_T1], *, return_exceptions: Literal[False] = False
+    coro: Coroutine[Any, Any, _T1], *, return_exceptions: Literal[False] = False
 ) -> tuple[_T1]:
     ...
 
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
     *,
     return_exceptions: Literal[False] = False,
 ) -> tuple[_T1, _T2]:
@@ -75,9 +64,9 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
-    __coro_or_future3: _CoroutineLike[_T3],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
+    __coro_or_future3: Coroutine[Any, Any, _T3],
     *,
     return_exceptions: Literal[False] = False,
 ) -> tuple[_T1, _T2, _T3]:
@@ -86,10 +75,10 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
-    __coro_or_future3: _CoroutineLike[_T3],
-    __coro_or_future4: _CoroutineLike[_T4],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
+    __coro_or_future3: Coroutine[Any, Any, _T3],
+    __coro_or_future4: Coroutine[Any, Any, _T4],
     *,
     return_exceptions: Literal[False] = False,
 ) -> tuple[_T1, _T2, _T3, _T4]:
@@ -98,11 +87,11 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
-    __coro_or_future3: _CoroutineLike[_T3],
-    __coro_or_future4: _CoroutineLike[_T4],
-    __coro_or_future5: _CoroutineLike[_T5],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
+    __coro_or_future3: Coroutine[Any, Any, _T3],
+    __coro_or_future4: Coroutine[Any, Any, _T4],
+    __coro_or_future5: Coroutine[Any, Any, _T5],
     *,
     return_exceptions: Literal[False] = False,
 ) -> tuple[_T1, _T2, _T3, _T4, _T5]:
@@ -111,12 +100,12 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
-    __coro_or_future3: _CoroutineLike[_T3],
-    __coro_or_future4: _CoroutineLike[_T4],
-    __coro_or_future5: _CoroutineLike[_T5],
-    __coro_or_future6: _CoroutineLike[_T6],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
+    __coro_or_future3: Coroutine[Any, Any, _T3],
+    __coro_or_future4: Coroutine[Any, Any, _T4],
+    __coro_or_future5: Coroutine[Any, Any, _T5],
+    __coro_or_future6: Coroutine[Any, Any, _T6],
     *,
     return_exceptions: Literal[False] = False,
 ) -> tuple[_T1, _T2, _T3, _T4, _T5, _T6]:
@@ -125,22 +114,23 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    *coros_or_futures: _CoroutineLike[_T], return_exceptions: Literal[False] = False
+    *coros_or_futures: Coroutine[Any, Any, _T],
+    return_exceptions: Literal[False] = False,
 ) -> list[_T]:
     ...
 
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1], *, return_exceptions: bool
+    __coro_or_future1: Coroutine[Any, Any, _T1], *, return_exceptions: bool
 ) -> tuple[_T1 | BaseException]:
     ...  # type: ignore[overload-overlap]
 
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
     *,
     return_exceptions: bool,
 ) -> tuple[_T1 | BaseException, _T2 | BaseException]:
@@ -149,9 +139,9 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
-    __coro_or_future3: _CoroutineLike[_T3],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
+    __coro_or_future3: Coroutine[Any, Any, _T3],
     *,
     return_exceptions: bool,
 ) -> tuple[_T1 | BaseException, _T2 | BaseException, _T3 | BaseException]:
@@ -160,10 +150,10 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
-    __coro_or_future3: _CoroutineLike[_T3],
-    __coro_or_future4: _CoroutineLike[_T4],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
+    __coro_or_future3: Coroutine[Any, Any, _T3],
+    __coro_or_future4: Coroutine[Any, Any, _T4],
     *,
     return_exceptions: bool,
 ) -> tuple[
@@ -177,11 +167,11 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
-    __coro_or_future3: _CoroutineLike[_T3],
-    __coro_or_future4: _CoroutineLike[_T4],
-    __coro_or_future5: _CoroutineLike[_T5],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
+    __coro_or_future3: Coroutine[Any, Any, _T3],
+    __coro_or_future4: Coroutine[Any, Any, _T4],
+    __coro_or_future5: Coroutine[Any, Any, _T5],
     *,
     return_exceptions: bool,
 ) -> tuple[
@@ -196,12 +186,12 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(  # type: ignore[overload-overlap]
-    __coro_or_future1: _CoroutineLike[_T1],
-    __coro_or_future2: _CoroutineLike[_T2],
-    __coro_or_future3: _CoroutineLike[_T3],
-    __coro_or_future4: _CoroutineLike[_T4],
-    __coro_or_future5: _CoroutineLike[_T5],
-    __coro_or_future6: _CoroutineLike[_T6],
+    __coro_or_future1: Coroutine[Any, Any, _T1],
+    __coro_or_future2: Coroutine[Any, Any, _T2],
+    __coro_or_future3: Coroutine[Any, Any, _T3],
+    __coro_or_future4: Coroutine[Any, Any, _T4],
+    __coro_or_future5: Coroutine[Any, Any, _T5],
+    __coro_or_future6: Coroutine[Any, Any, _T6],
     *,
     return_exceptions: bool,
 ) -> tuple[
@@ -217,13 +207,13 @@ async def gather(  # type: ignore[overload-overlap]
 
 @overload
 async def gather(
-    *coros_or_futures: _CoroutineLike[_T], return_exceptions: bool
+    *coros_or_futures: Coroutine[Any, Any, _T], return_exceptions: bool
 ) -> list[_T | BaseException]:
     ...
 
 
 async def gather(  # type: ignore[misc]
-    *coros: _CoroutineLike, return_exceptions: bool = False
+    *coros: Coroutine, return_exceptions: bool = False
 ) -> tuple:
     """A safer version of `asyncio.gather`.
 
@@ -232,8 +222,8 @@ async def gather(  # type: ignore[misc]
 
     Notable differences are:
 
-    * `quattro.gather` only accepts coroutines and not futures, just like a
-      TaskGroup.
+    * `quattro.gather` only accepts coroutines and not futures and
+      generators, just like a TaskGroup.
     * When `return_exceptions` is false (the default), an exception in a child task
       will cause an ExceptionGroup to bubble out of the top-level `gather()` call,
       just like in a TaskGroup.
@@ -254,7 +244,7 @@ async def gather(  # type: ignore[misc]
     return tuple([await f for f in subtasks])
 
 
-async def _wrap_coro(coro: _CoroutineLike[_T]) -> _T | BaseException:
+async def _wrap_coro(coro: Coroutine[Any, Any, _T]) -> _T | BaseException:
     """Adapt a raised exception into the return value."""
     try:
         return await coro
