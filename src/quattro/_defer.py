ca0ec506b698d72696a517756f6a0518f326347c
Tin Tvrtković <tinchester@gmail.com>
2025-03-01 22:27:34 +0100

    Docs

--- src/quattro/_defer.py
+++ src/quattro/_defer.py
@@ -19,6 +19,35 @@ Aw = TypeVar("Aw", bound=Awaitable)
 
 
 class Defer(AsyncExitStack):
+    """A decorator and class to enable deferring functions until the end of a coroutine.
+
+    First, apply the `Defer.defer` decorator to a coroutine function.
+    The coroutine function will receive an instance of `Defer` as its first positional
+    argument.
+
+    This instance of `Defer` can be used to enter async context managers; these context
+    managers will be exited after the coroutine finishes.
+
+    Example:
+        >>> from quattro import Defer
+
+        >>> @Defer.defer
+        ... async def test(defer: Defer) -> None:
+        ...     tg = defer(TaskGroup())  # This TaskGroup will be exited after return
+        ...
+
+    `Defer` is a subclass of `contextlib.AsyncExitStack`, so it supports its full API:
+    * `AsyncExitStack.enter_async_context` (equivalent to just `Defer.__call__` shown
+        above)
+    * `AsyncExitStack.push_async_callback`
+    * `AsyncExitStack.push_async_exit`
+    * `BaseExitStack.enter_context`
+    * `BaseExitStack.callback`
+    * `BaseExitStack.push`
+
+    Loosely inspired by the Go `defer` statement. (https://go.dev/tour/flowcontrol/12)
+    """
+
     @overload
     async def __call__(self, cm: AbstractAsyncContextManager[T], /) -> T: ...
 
@@ -75,6 +104,8 @@ class Defer(AsyncExitStack):
     async def __call__(self, *cms):
         """A quick alias for `self.enter_async_context`.
 
+        Enter an async context manager, and schedule its exit for later.
+
         Accepts multiple async context managers.
         """
         if len(cms) == 1:
@@ -83,6 +114,12 @@ class Defer(AsyncExitStack):
 
     @classmethod
     def defer(cls, function: "Callable[Concatenate[Defer, P], Aw]") -> Callable[P, Aw]:
+        """A decorator to be applied to a coroutine function.
+
+        The coroutine function should receive an instance of `Defer` as its first
+        positional argument; this will be provided by `Defer`.
+        """
+
         async def inner(*args, **kwargs):
             defer = cls()
             async with defer:
